f3b5d7c12dfab77c6a1c1b84a17e5857fd19ac33
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-07-30 18:38:33 -0700

    added venus test, it fails.

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -2,13 +2,19 @@ import Years from './../src/index.js';
 
 describe('Years', () => {
   test('shows age in earth years', () => {
-    let earthYears = new Years(20);
+    const earthYears = new Years(20);
     expect(earthYears.age).toEqual(20);
   });
 
   test('convert earth age to Mercury years', () => {
-    let ageOne = new Years(20);
-    let pick = "mercury";
-    expect(ageOne.ConvertAge(pick)).toEqual(4.8);
+    const age = new Years(20);
+    const pick = "mercury";
+    expect(age.ConvertAge(pick)).toEqual(4.8);
   });
+  test('convert earth age to Venus years', () => {
+    const age = new Years(20);
+    const pick = "venus";
+    expect(age.ConvertAge(pick)).toEqual(12.4);
+  });
+
 });
