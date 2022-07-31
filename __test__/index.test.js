2130d64ebf7670c695d8a2ae40a970d9116cc6e5
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-07-30 18:30:55 -0700

    added mercury test, it fails.

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -3,7 +3,12 @@ import Years from './../src/index.js';
 describe('Years', () => {
   test('shows age in earth years', () => {
     let earthYears = new Years(20);
-    console.log(earthYears);
     expect(earthYears.age).toEqual(20);
   });
-})
+
+  test('convert earth age to Mercury years', () => {
+    let ageOne = new Years(20);
+    let pick = "mercury";
+    expect(ageOne.ConvertAge(pick)).toEqual(4.8);
+  });
+});
